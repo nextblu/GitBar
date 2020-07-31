0eaf17ada67096affcce6d4a56dd819878121431
Mattia <mattiafailla@hotmail.it>
2020-07-31 17:33:21 +0200

    Adding base template

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'gitbar'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
