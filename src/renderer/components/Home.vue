e53fe850fe0df71410d88c19a080a2da0344ec1f
Mattia <mattiafailla@hotmail.it>
2020-07-31 22:50:32 +0200

    Rewriting home

--- src/renderer/components/Home.vue
+++ src/renderer/components/Home.vue
@@ -1,29 +1,101 @@
 <template>
     <div class="app">
+        <Topbar></Topbar>
 
-        <div class="container is-fullhd">
+        <div v-if="loading">
+            <div class="container is-fluid">
 
-
-            <b-message title="Default" aria-close-label="Close message">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id fermentum quam. Proin sagittis, nibh id hendrerit imperdiet, elit sapien laoreet elit
-            </b-message>
-
+                <section class="loadeing-animation">
+                    <article class="media" v-for="i in media" :key="i">
+                        <figure class="media-left">
+                            <p class="image is-64x64">
+                                <b-skeleton circle width="64px" height="64px"></b-skeleton>
+                            </p>
+                        </figure>
+                        <div class="media-content">
+                            <div class="content">
+                                <p>
+                                    <b-skeleton active></b-skeleton>
+                                    <b-skeleton  height="80px"></b-skeleton>
+                                </p>
+                            </div>
+                            <nav class="level is-mobile">
+                                <div class="level-left">
+                                    <a class="level-item">
+                                <span class="icon is-small">
+                                    <b-skeleton></b-skeleton>
+                                </span>
+                                    </a>
+                                    <a class="level-item">
+                                <span class="icon is-small">
+                                    <b-skeleton></b-skeleton>
+                                </span>
+                                    </a>
+                                </div>
+                            </nav>
+                        </div>
+                    </article>
+                </section>
+            </div>
+        </div>
+        <div v-else>
+            <section>
+                <article class="media">
+                    <figure class="media-left">
+                        <p class="image is-64x64">
+                            <img class="is-rounded" src="logo.png">
+                        </p>
+                    </figure>
+                    <div class="media-content">
+                        <div class="content">
+                            <p>
+                                Corpo del testo della notifica
+                            </p>
+                        </div>
+                        <nav class="level is-mobile">
+                            <div class="level-left">
+                                <a class="level-item">
+                                <span class="icon is-small">
+                                    <b-icon
+                                            icon="account"
+                                            size="is-small">
+                                    </b-icon>
+                                </span>
+                                </a>
+                                <a class="level-item">
+                                <span class="icon is-small">
+                                    <b-icon
+                                            icon="account"
+                                            size="is-small">
+                                    </b-icon>
+                                </span>
+                                </a>
+                            </div>
+                        </nav>
+                    </div>
+                </article>
+            </section>
 
         </div>
+
+
         <Navbar></Navbar>
     </div>
 </template>
 
 <script>
     import Navbar from './Home/Bottombar'
+    import Topbar from './Home/Topbar'
 export default {
       name: 'home-page',
       components: {
-        Navbar
+        Navbar,
+        Topbar
       },
       data () {
         return {
-
+          loading: true,
+          media: 7
         }
       },
       mounted () {
@@ -38,5 +110,11 @@ export default {
 </script>
 
 <style scoped>
+    .media {
+        padding: 40px;
+    }
+    .loadeing-animation {
+        margin-top: 40px;
+    }
 
 </style>
