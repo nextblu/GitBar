ea00a825ff716e67e5cc212df138c448195f8225
Mattia <mattiafailla@hotmail.it>
2020-07-31 21:18:42 +0200

    Improving Home
    Adding the navbar components & snackbar on mounted()

--- src/renderer/components/Home.vue
+++ src/renderer/components/Home.vue
@@ -1,10 +1,39 @@
 <template>
-    
+    <div class="app">
+
+        <div class="container is-fullhd">
+
+
+            <b-message title="Default" aria-close-label="Close message">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id fermentum quam. Proin sagittis, nibh id hendrerit imperdiet, elit sapien laoreet elit
+            </b-message>
+
+
+        </div>
+        <Navbar></Navbar>
+    </div>
 </template>
 
 <script>
-    export default {
-        name: "Home.vue"
+    import Navbar from './Home/Navbar'
+export default {
+      name: 'home-page',
+      components: {
+        Navbar
+      },
+      data () {
+        return {
+
+        }
+      },
+      mounted () {
+        this.$buefy.snackbar.open({
+          message: 'Welcome back!',
+          type: 'is-warning',
+          position: 'is-bottom',
+          indefinite: false
+        })
+      }
     }
 </script>
 
